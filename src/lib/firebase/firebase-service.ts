11009df7e6e8d98f535d618f75a8c8555898e775
Nicola <lanzoni.nicola@gmail.com>
2022-05-30 18:11:16 -0300

    Fixing Firebase Private Key error on Netlify build process

--- src/lib/firebase/firebase-service.ts
+++ src/lib/firebase/firebase-service.ts
@@ -3,7 +3,7 @@ import { cert, initializeApp } from "firebase-admin/app";
 const firebaseConfig = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 };
 
 // Initialize Firebase
