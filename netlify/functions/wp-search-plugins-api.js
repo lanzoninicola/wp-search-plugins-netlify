60921f8f5fa1011222e032a29d3c15e9a181d8a9
Nicola <lanzoni.nicola@gmail.com>
2022-05-30 15:11:53 -0300

    Update function

--- netlify/functions/wp-search-plugins-api.js
+++ netlify/functions/wp-search-plugins-api.js
@@ -1,6 +1,27 @@
+// https://github.com/marketplace/actions/http-request-action
+// https://jasonet.co/posts/scheduled-actions/
+// https://prismatic-yeot-e7f71f.netlify.app/.netlify/functions/wp-search-plugins-api
+
 exports.handler = async function (event, context) {
+  const plugins = await getWordpressPlugins();
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
+    body: JSON.stringify(plugins),
   };
 };
+
+async function getWordpressPlugins() {
+  const url =
+    "https://api.wordpress.org/plugins/info/1.2/?action=query_plugins";
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  return fetch(url, options)
+    .then((response) => response.json())
+    .then((json) => json.plugins)
+    .catch((error) => console.log(error));
+}
